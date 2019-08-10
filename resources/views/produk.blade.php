ca964e8c57db27a07a8db3de4fe5a52e546f6243
aribahtiarpro <aribahtiarpro@gmail.com>
2019-08-10 12:37:07 +0700

    tt

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -226,7 +226,7 @@
 
     function lihatDetail(d){
         // console.log(data);
-        let modalDetail = `<div class="modal animated bounceInDown" id="modalD">
+        let modalDetail = `<div class="modal animated bounceInUp" id="modalD">
                             <div class="modal-dialog modal-dialog-centered modal-lg">
                                 <div class="modal-content p-lg-4 btn-circle">
                                         <div class="col-md-12">
